88f4b0e2891b5642d0be3191e0009efa6f285925
Luan Lopes <luan.lps01@gmail.com>
2021-04-06 23:43:14 -0300

    Added a way to hide credentials and a .gitignore

--- src/trello.py
+++ src/trello.py
@@ -1,35 +1,42 @@
 import requests as r 
 import json as j
 import pprint as p
+import os
+
+current_dir = os.getcwd()
+
+CREDENTIALS = current_dir + "/creds/04062021.json"
+
+data = j.load(open(CREDENTIALS, 'r'))
 
 class Trello:
     def __init__(self):
-        self.key = 'YOURKEY'
-        self.token = 'YOURAPITOKEN'
-        self.board = 'BOARDID'
-        self.list = 'LISTID'
-        self.surl = 'https://api.trello.com/1'
+        self.key = data['trello']['key']
+        self.token = data['trello']['token']
+        self.board = data['trello']['board']
+        self.list = data['trello']['list']
+        self.baseurl = 'https://api.trello.com/1'
         self.query = {
             'key': self.key,
             'token': self.token
         }
     
     def get_board(self):
-        url = f'{self.surl}/boards/{self.board}'
+        url = f'{self.baseurl}/boards/{self.board}'
 
         response = r.request('GET', url, params=self.query)
         
         p.pprint(j.loads(response.text))
     
     def get_board_cards(self):
-        url = f'{self.surl}/boards/{self.board}/cards'
+        url = f'{self.baseurl}/boards/{self.board}/cards'
 
         response = r.request('GET', url, params=self.query)
         
         p.pprint(j.loads(response.text))
     
     def get_board_lists(self):
-        url = f'{self.surl}/boards/{self.board}/lists'
+        url = f'{self.baseurl}/boards/{self.board}/lists'
 
         response = r.request('GET', url, params=self.query)
         
@@ -38,14 +45,14 @@ class Trello:
     def get_card(self):
         card = 'CARDID'
 
-        url = f'{self.surl}/cards/{card}'
+        url = f'{self.baseurl}/cards/{card}'
 
         response = r.request('GET', url, params=self.query)
         
         p.pprint(j.loads(response.text))
 
     def create_card(self, name, desc):
-        url = f'{self.surl}/cards/'
+        url = f'{self.baseurl}/cards/'
 
         query = {
             'key': self.key,
--- src/zendesk.py
+++ src/zendesk.py
@@ -1,17 +1,24 @@
 import requests as r 
 import json as j
+import os
+
+current_dir = os.getcwd()
+
+CREDENTIALS = current_dir + "/creds/04062021.json"
+
+data = j.load(open(CREDENTIALS, 'r'))
 
 class Zendesk:
     def __init__(self):
-        self.user = 'your@email/token'
-        self.token = 'YOURAPITOKEN'
-        self.surl = 'https://yourdomain.zendesk.com/api/v2'
+        self.user = data['zendesk']['user']
+        self.token = data['zendesk']['token']
+        self.baseurl = data['zendesk']['baseurl']
         self.headers = {
             'content-type': 'application/json'
         }
 
     def get_ticket(self, id):
-        url = f'{self.surl}/tickets/{id}'
+        url = f'{self.baseurl}/tickets/{id}'
 
         response = r.request('GET', url, auth=(self.user, self.token), headers=self.headers)
 
