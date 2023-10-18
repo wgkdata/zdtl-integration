a71d25471d154ee00d7a7b1d45128c47e0b4403a
Luan Lopes <luan.lps01@gmail.com>
2023-10-18 00:45:22 -0300

    Test change

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import requests as r 
+import requests as r
 import json as j
 from trello import Trello
 from zendesk import Zendesk
@@ -6,12 +6,14 @@ from zendesk import Zendesk
 trl = Trello()
 zd = Zendesk()
 
+
 def create_ticket_card(id):
-   tk = zd.get_ticket(id)
-   ticket_name = f"{id} - {tk['ticket']['subject']}"
-   ticket_desc = tk['ticket']['description']
+    tk = zd.get_ticket(id)
+    ticket_name = f"{id} - {tk['ticket']['subject']}"
+    ticket_desc = tk['ticket']['description']
 
-   response = trl.create_card(ticket_name, ticket_desc)
+    response = trl.create_card(ticket_name, ticket_desc)
 
-   if response == r.codes.ok:
-      print(f'Card do ticket {id} criado com sucesso.')
+    if response == r.codes.ok:
+        print(f'Card do ticket {id} criado com sucesso.')
+# Test
